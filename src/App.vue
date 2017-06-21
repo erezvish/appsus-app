b69fec5dcedadd981ae8c956b5297b65dbdada0d
Erez Vishnia <erezvish@gmail.com>
2017-06-21 22:45:26 +0300

    Initial project status

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader'
+export default {
+  name: 'app',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <div class="placeholder"></div>
+      <h1> E.T. Mail! </h1>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'app-header'
+
+}
+</script>
+
+<style lang="scss" scoped>
+.placeholder {
+    height: 50px;
+    background-color: lightblue;
+}
+
+section {
+    h1 {
+        color: darkblue;
+    }
+}
+</style>

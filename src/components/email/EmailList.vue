91ab0ed6828694197780ee28b598524082f45592
Erez Vishnia <erezvish@gmail.com>
2017-06-22 13:25:23 +0300

    merging

--- src/components/email/EmailList.vue
+++ src/components/email/EmailList.vue
@@ -1,13 +1,19 @@
 <template>
     <section>
         <h1>Email List </h1>
-        <button @click.stop="printMails()">print </button>
+        <ul v-if="mails.length">
+            <email-preview v-for="email in mails" :email="email"> </email-preview>
+        </ul>
     </section>
 </template>
 
 <script>
+import EmailPreview from './EmailPreview'
 export default {
     name: 'email-list',
+    components: {
+        EmailPreview
+    },
     props: ['mails'],
     methods: {
         printMails() {

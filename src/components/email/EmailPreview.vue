cef6a59547dff64b2633c2f4ac7c082290dd4582
Erez Vishnia <erezvish@gmail.com>
2017-06-22 12:33:12 +0300

    email going down to preview plus font awesome

---
+++ src/components/email/EmailPreview.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>
+        <div>
+            <div>
+                <i class="fa fa-envelope-o"></i>
+                <p class="preview-subject">subject: {{email.subject}} </p>
+            </div>
+            <p class="preview-body">subject: {{email.body}} </p>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'email-preview',
+    props: ['email']
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

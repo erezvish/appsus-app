83252c50a45981a74e198c4229b2131a0a322e3a
Tamiross <tamir.mbox@gmail.com>
2017-06-22 09:47:35 +0300

    ESlis is disabled!

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <app-header></app-header>
+    
     <router-view></router-view>
   </div>
 </template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>nav</h1>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+
+<style>
+
+</style>

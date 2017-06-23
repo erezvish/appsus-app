adee8274dceb5e69b3ba07e9a3967bb198076dd7
Erez Vishnia <erezvish@gmail.com>
2017-06-23 22:04:44 +0300

    We have a map!

--- src/components/map/AppMap.vue
+++ src/components/map/AppMap.vue
@@ -1,6 +1,47 @@
 <template>
     <section>
-        <div class="placeholder"></div>
-      <h1>Map Page </h1>
+        <h1>Managing your Places - 24/7 </h1>
+        <div class="map-area">
+            <gmap-map :center="center" :zoom="7" style="width: 90%; height: 60vh">
+                <gmap-marker :key="index" v-for="(m, index) in markers" :position="m.position" :clickable="true" :draggable="true" @click="center=m.position"></gmap-marker>
+            </gmap-map>
+        </div>
     </section>
 </template>
+
+<script>
+import * as VueGoogleMaps from 'vue2-google-maps';
+import Vue from 'vue';
+
+Vue.use(VueGoogleMaps, {
+    load: {
+        key: 'AIzaSyAppatuE2pTnbbwnUSUm2hZiIzstrPuSt8',
+        // libraries: 'places', //// If you need to use place input
+    }
+});
+
+export default {
+    data() {
+        return {
+            center: { lat: 32.1, lng: 34.8 },
+            markers: [{
+                position: { lat: 32.1, lng: 34.8 }
+            }, {
+            }]
+        }
+    }
+}
+</script>
+</script>
+
+<style lang="scss" scoped>
+.map-area {
+    display: flex;
+    justify-content: center;
+}
+
+h1 {
+    text-align: center;
+}
+</style>
+

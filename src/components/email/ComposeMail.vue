bbf6ef38a036c4e6eccce159bb4a6e19aa229b05
Tamiross <tamir.mbox@gmail.com>
2017-06-23 13:17:41 +0300

    commit before pulling

--- src/components/email/ComposeMail.vue
+++ src/components/email/ComposeMail.vue
@@ -2,43 +2,14 @@
   <section class="btn-compose">
     <el-button type="warning" @click.stop="createNewMail()">New mail</el-button>
     
-    <el-popover
+    <!--<el-popover
   ref="popover4"
   placement="right"
   width="400"
   trigger="click">
 
-    <el-form 
-      :model="ruleForm2" 
-      :rules="rules2" 
-      ref="ruleForm2" 
-      label-width="120px" 
-      class="demo-ruleForm">
 
-      <el-input
-        type="textarea"
-        autosize
-        placeholder="Subject"
-        v-model="subject-input">
-      </el-input>
-     
-      <div style="margin: 20px 0;"></div>
-      
-      <el-input
-        type="textarea"
-        :autosize="{ minRows: 15, maxRows: 24}"
-        placeholder="Message..."
-        v-model="body-input">
-      </el-input>
-      <div style="margin: 20px 0;"></div>
-      <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm2')">Submit</el-button>
-        <el-button @click="resetForm('ruleForm2')">Reset</el-button>
-      </el-form-item>
-  </el-form>
-
-
-</el-popover>
+</el-popover>-->
 
 <el-button v-popover:popover4>New mail</el-button>
 
@@ -49,24 +20,10 @@
 
 export default {
   name: 'compose-mail',
-  data() {
-    return {
-      subject-input: ''
-      body-input: '',
-    }
-  },
   methods: {
     createNewMail() {
       this.$emit('compose-mail');
       
-    },
-    resetForm(ruleForm2) {
-      console.log('reset form...')
-      this.$refs[formName].resetFields();
-    },
-    submitForm(ruleForm2) {
-      console.log('submit form...')
-      this.$refs[formName].resetFields();
     }
   }
 }
